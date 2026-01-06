e6fd13485b5ea4085a550531be53fd3f42a20f4f
Johnrae R. Albay <134664542+johnraealbay@users.noreply.github.com>
2026-01-06 10:35:06 +0800

    Enhance navbar and add typing effect
    Added typing effect for developer roles and improved navbar functionality.

--- My Portfolio/js/script.js
+++ My Portfolio/js/script.js
@@ -43,6 +43,31 @@ window.addEventListener('load', () => {
     });
 });
 
+const texts = ["Junior Developer!", "Web Developer!", "Frontend Developer!"];
+  let textIndex = 0;
+  let charIndex = 0;
+  const speed = 100;
+
+  function typeText() {
+    if (charIndex < texts[textIndex].length) {
+      document.getElementById("typing-text").textContent += texts[textIndex].charAt(charIndex);charIndex++;
+      setTimeout(typeText, speed);
+    } else {
+      setTimeout(eraseText, 1500);
+    }
+  }
+
+  function eraseText() {
+    if (charIndex > 0) {
+      document.getElementById("typing-text").textContent = texts[textIndex].substring(0, charIndex - 1); charIndex--;
+      setTimeout(eraseText, speed / 2);
+    } else {
+      textIndex = (textIndex + 1) % texts.length;
+      setTimeout(typeText, 500);
+    }
+  }
+
+  typeText();
 
 
 // ===== scroll reveal ===== //
@@ -100,4 +125,5 @@ ScrollReveal().reveal('.about-img', {
   easing: 'ease-in-out',
   reset: true        // animate again when scrolling back
 });
+
 
